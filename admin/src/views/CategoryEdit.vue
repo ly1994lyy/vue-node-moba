942d4e24fa7d4e17bd5c8a1850843f8ea6693c1c
ly1994lyy <1141730852@qq.com>
2019-06-15 16:21:24 +0800

    分类的增删改查接口以及样式

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <h2>新建分类</h2>
+        <h2>{{id ? '编辑':'新建'}}分类</h2>
         <el-form @submit.native.prevent="save">
+            <el-form-item label='上级名称'>
+                <el-select v-model="model.parent">
+                    <el-option></el-option>
+                </el-select>
+            </el-form-item>
             <el-form-item label='名称'>
                 <el-input v-model="model.name"></el-input>
             </el-form-item>
@@ -14,15 +19,35 @@
 
 <script>
 export default {
+    props:{
+        id:{}
+    },
     data (){
         return{
             model:{}
         }
     },
     methods: {
-        save() {
-
+        async save() {
+            let res
+            if (this.id){
+                res = await this.$http.put(`categories/${this.id}`,this.model)
+            }else{
+                res = await this.$http.post('categories',this.model)
+            }
+            this.$router.push('/categories/list')
+            this.$message({
+                type:'success',
+                message:'保存成功'
+            })
+        },
+        async fetch() {
+            const res = await this.$http.get(`categories/${this.id}`)
+            this.model = res.data
         }
+    },
+    created(){
+        this.id&&this.fetch()
     }
 }
 </script>
