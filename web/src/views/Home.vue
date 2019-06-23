2210a6d44146e0586762cf781337c40a3412c8c6
ly1994lyy <1141730852@qq.com>
2019-06-23 22:45:14 +0800

    中间菜单

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -12,6 +12,54 @@
       </swiper-slide>
       <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
     </swiper>
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-drak-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-story"></i>
+          <div class="py-2">故事站</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-shop"></i>
+          <div class="py-2">周边商城</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-tiyanfu"></i>
+          <div class="py-2">体验服</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-newpeople"></i>
+          <div class="py-2">新人专区</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-rongyao"></i>
+          <div class="py-2">荣耀·传承</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-tongren"></i>
+          <div class="py-2">同人社区</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-yingdi"></i>
+          <div class="py-2">王者营地</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-gongzhonghao"></i>
+          <div class="py-2">公众号</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-story"></i>
+          <div class="py-2">版本介绍</div>
+        </div>
+      </div>  
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>  
+      </div>   
+    </div>
   </div>
 </template>
 
@@ -41,6 +89,18 @@ export default {
     background: #fff;
     &.swiper-pagination-bullet-active{
       background: map-get($colors , 'info');
+    }
+  }
+}
+
+.nav-icons{
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item{
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n){
+      border-right: none;
     }
   }
 }
