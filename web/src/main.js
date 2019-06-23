9d5754c58a84456e607a6565cebd8430b94367a0
ly1994lyy <1141730852@qq.com>
2019-06-23 17:33:15 +0800

    轮播图和顶部菜单

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 
 Vue.config.productionTip = false
 
-import './style.scss'
+import './assets/scss/style.scss'
+import router from './router'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/cf472f06627e131168b925685f6ff292.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/a05ce9c0264ba30a9d0892e733bc6874.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/ce7c7cb7fed83a9f657ee914a53e3f20.jpeg" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data(){
+    return{
+      swiperOption: {
+        pagination: {
+            el: '.pagination-home'
+          }
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.pagination-home{
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: #fff;
+    &.swiper-pagination-bullet-active{
+      background: map-get($colors , 'info');
+    }
+  }
+}
+</style>
