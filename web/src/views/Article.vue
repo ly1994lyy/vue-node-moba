c73c1a5146b2260889e8c712462bbc278454de6e
ly1994lyy <1141730852@qq.com>
2019-07-10 16:52:00 +0800

    文章详情页优化

--- web/src/views/Article.vue
+++ web/src/views/Article.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="page-article" v-if="model">
     <div class="d-flex py-3 px-2">
-      <div class="iconfont icon-fanhui"></div>
-      <strong class="flex-1">
+      <div class="iconfont icon-fanhui text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">
         {{model.title}}
       </strong>
       <div class="text-grey fs-xs">2019-6-25</div>
     </div>
-    <div v-html="model.body"></div>
+    <div v-html="model.body" class="px-3 body fs-lg"></div>
+    <div class="px-3 border-top py-2">
+      <div class="d-flex ai-center">
+        <i class="iconfont icon-fanhui"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+      <div class="pt-1 fs-lg">
+        <router-link class="py-2" tag='div' :to='`/articles/${item._id}`' v-for="item in model.related" :key="item._id">
+          {{item.title}}
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +40,27 @@ export default {
   },
   created(){
     this.fetch()
+  },
+  watch:{
+    id:'fetch'
   }
 }
 </script>
+
+<style lang="scss">
+.page-article{
+  .icon-fanhui{
+    font-size: 1.6923rem
+  }
+  .body{
+    img{
+      max-width: 100%;
+      height: auto;
+    }
+    iframe{
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
